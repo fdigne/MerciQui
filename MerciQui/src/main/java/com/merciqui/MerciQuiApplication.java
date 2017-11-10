c74dbf6a460a17d1f062f76ef7d791f5f9ff5b97
fdigne <fdigne@me.com>
2017-11-10 20:30:00 +0100

    First commit

---
+++ MerciQui/src/main/java/com/merciqui/MerciQuiApplication.java
@@ -0,0 +1,12 @@
+package com.merciqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerciQuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerciQuiApplication.class, args);
+	}
+}
