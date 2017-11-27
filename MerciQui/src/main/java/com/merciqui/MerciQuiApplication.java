9465a26a826149e28a2b40852cd77ac3ef867e7b
fdigne <fdigne@me.com>
2017-11-27 16:45:13 +0100

    Resolve #4

--- MerciQui/src/main/java/com/merciqui/MerciQuiApplication.java
+++ MerciQui/src/main/java/com/merciqui/MerciQuiApplication.java
@@ -3,12 +3,14 @@ package com.merciqui;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.merciqui.dao.ComedienRepository;
 import com.merciqui.entities.Comedien;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MerciQuiApplication implements CommandLineRunner{
 	
---
+++ MerciQui/src/main/java/com/merciqui/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.merciqui.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    public SecurityWebApplicationInitializer() {
+        super(SecurityConfig.class);
+    }
+
+}
