07126ca9949ec18b6345279aec38adb39fd64afd
Florian Digne <fdigne@me.com>
2019-04-16 22:01:49 +0200

    Ajout periode filtre pour la vue

--- MerciQui/src/main/java/com/merciqui/web/MultipleEventsController.java
+++ MerciQui/src/main/java/com/merciqui/web/MultipleEventsController.java
@@ -2,19 +2,15 @@ package com.merciqui.web;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
@@ -23,10 +19,8 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
-import com.merciqui.entities.Comedien;
-import com.merciqui.entities.Evenement;
+import com.merciqui.entities.Periode;
+import com.merciqui.entities.PeriodeFiltre;
 import com.merciqui.metier.IMerciQuiMetier;
 
 @Controller
@@ -59,11 +53,31 @@ public class MultipleEventsController {
 	}
 
 	@RequestMapping("/multipleEvents")
-	public String ajouterMultipleEvents(Model model) throws IOException, GeneralSecurityException {
-
+	public String ajouterMultipleEvents(Model model) {
+		Collection<PeriodeFiltre> listePeriodesFiltres = merciquimetier.listePeriodeFiltre();
+		model.addAttribute("listePeriodesFiltres", listePeriodesFiltres);
+		return "MultipleEventsView";
+	}
+	
+	@RequestMapping("/multipleEvents/periodeFiltre")
+	public String ajouterMultipleEventsPeriode(Model model, Long idPeriodeFiltre) throws GeneralSecurityException, IOException {
+		Periode periodeFiltre = merciquimetier.consulterPeriode(idPeriodeFiltre);
+		//int numberOfDays = this.getNumberOfDays(periodeFiltre);
+		model.addAttribute("days", idPeriodeFiltre);
 		client = new com.google.api.services.calendar.Calendar.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, GestionAgendaController.credential)
 				.setApplicationName(APPLICATION_NAME).build();
 
 		return "MultipleEventsView";
 	}
+
+	private int getNumberOfDays(Periode periodeFiltre) {
+		Calendar cal1 = Calendar.getInstance();
+		Calendar cal2 = Calendar.getInstance();
+		cal1.setTime(periodeFiltre.getDateDebut());
+		cal2.setTime(periodeFiltre.getDateFin());
+		
+		return (int) ((cal2.getTimeInMillis() - cal1.getTimeInMillis()) / (1000 * 60 * 60 * 24));
+	}
+	
+	
 }
