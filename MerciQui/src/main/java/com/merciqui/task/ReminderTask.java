f23288ea0b111db1f75adaa0de6047ea779e775d
Florian Digne <fdigne@me.com>
2019-12-29 21:20:30 +0100

    Add cron send SMS everyday

--- MerciQui/src/main/java/com/merciqui/task/ReminderTask.java
+++ MerciQui/src/main/java/com/merciqui/task/ReminderTask.java
@@ -2,10 +2,9 @@ package com.merciqui.task;
 
 import com.merciqui.entities.Comedien;
 import com.merciqui.entities.Evenement;
-import com.merciqui.entities.PeriodeFiltre;
-import com.merciqui.entities.Spectacle;
 import com.merciqui.metier.IMerciQuiMetier;
 import com.merciqui.web.GestionMailsController;
+import com.twilio.Twilio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Component;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import java.math.BigInteger;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,17 +19,57 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Properties;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+
 @Component
 public class ReminderTask {
 
     private final String fromEmail = "les3tcafetheatreagenda@gmail.com"; //requires valid gmail id
     private final String password = "les3tcafetheatre"; // correct password for gmail id
 
+    public static final String ACCOUNT_SID = "AC5004a7c3df22a2936423fb717614709a";
+    public static final String AUTH_TOKEN = "c7a878ea2ada71f5b49289aa8eccd414";
+
+
     @Autowired
     IMerciQuiMetier merciquimetier ;
 
+    @Scheduled(cron = "0 0 18 * * *")
+    public void sendDailyReminder() {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR, 1);
+        today.set(Calendar.MINUTE, 0);
+        today.add(Calendar.DATE, 1);
+
+        Date dateDebut = today.getTime();
+        today.add(Calendar.DATE, 1);
+        Date dateFin = today.getTime();
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        for (Comedien comedien : merciquimetier.getListeComediensParPeriode(dateDebut, dateFin)) {
+            if (comedien.getNumTel() != null && !comedien.getNumTel().isEmpty()) {
+                String numTel = comedien.getNumTel();
+                numTel = numTel.replaceAll("\\s+","");
+                if (!numTel.startsWith("+33")) {
+                    numTel = numTel.replaceFirst("0","+33");
+                }
+                    Message message = Message.creator(new PhoneNumber(numTel), new PhoneNumber("+17543335908"),
+                            this.getSMSBody(comedien, merciquimetier.listeEvenementsParComedienParPeriode(comedien.getId3T(), dateDebut, dateFin), dateDebut)).create();
+                    System.out.println("SMS envoyé à " + comedien.getPrenomPersonne() + " " + comedien.getNomPersonne());
+                    System.out.println("SID: "+ message.getSid());
+            } else {
+                System.out.println("Le SMS n'a pas pu être envoyé à " + comedien.getPrenomPersonne() + " " + comedien.getNomPersonne());
+            }
+        }
+
+    }
+
     @Scheduled(cron = "0 0 17 * * SUN")
-    public void sendReminder() {
+    public void sendWeeklyReminder() {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -97,6 +135,25 @@ public class ReminderTask {
 
                     body += "<tr><td>"+dateEv+"</td><td>" + evenement.getSpectacle().getNomSpectacle() + "</td><td>"+
                             evenement.getNomSalle()+"</td></tr>";
+        }
+
+        return body;
+    }
+
+    private String getSMSBody(Comedien com, Collection<Evenement> listeEvenements, Date dateDebut) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String completeDate = df.format(dateDebut);
+        String body = "Bonjour " + com.getPrenomPersonne() +",\nTu as " + listeEvenements.size() + " représentation(s) demain le "
+                + completeDate + ": \n";
+        for (Evenement evenement : listeEvenements) {
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+            String dateEv = dateFormat.format(evenement.getDateEvenement());
+            body += dateEv + " " + evenement.getSpectacle().getNomSpectacle() + "\n";
+        }
+        if (com.getId3T() == 33) {
+            body += "Je t'aime.\nTon doudou qui t'écrit un message spécial que pour toi.";
+        } else {
+            body += "A demain.\nLaurence.";
         }
 
         return body;
