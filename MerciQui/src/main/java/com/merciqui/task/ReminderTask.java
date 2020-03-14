42c3c831747688f917d8e6d2493b7cce2f17c27d
Florian Digne <fdigne@me.com>
2020-03-14 20:46:23 +0100

    Add task activation status

--- MerciQui/src/main/java/com/merciqui/task/ReminderTask.java
+++ MerciQui/src/main/java/com/merciqui/task/ReminderTask.java
@@ -18,7 +18,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
@@ -34,6 +33,8 @@ public class ReminderTask {
 
     public String ACCOUNT_SID;
     public String AUTH_TOKEN;
+    public String TASK_STATUS;
+
     @Autowired
     IMerciQuiMetier merciquimetier ;
 
@@ -44,38 +45,43 @@ public class ReminderTask {
         initTwilioToken();
         initMailAccount();
 
-        Calendar today = Calendar.getInstance();
-        today.set(Calendar.HOUR, 1);
-        today.set(Calendar.MINUTE, 0);
-        today.add(Calendar.DATE, 1);
+        if (this.TASK_STATUS.equals("ACTIVATED")) {
 
-        Date dateDebut = today.getTime();
-        today.add(Calendar.DATE, 1);
-        Date dateFin = today.getTime();
-        System.out.println("TWILIO ACCOUNT: " + ACCOUNT_SID);
-        System.out.println("TOKEN: " + AUTH_TOKEN);
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Map<String, String> mapComediensSMS = new HashMap<>();
-        Collection<Comedien> listeComediensNonPrevenus = new ArrayList<>();
-        for (Comedien comedien : merciquimetier.getListeComediensParPeriode(dateDebut, dateFin)) {
-            if (comedien.getNumTel() != null && !comedien.getNumTel().isEmpty()) {
-                String numTel = comedien.getNumTel();
-                numTel = numTel.replaceAll("\\s+","");
-                if (!numTel.startsWith("+33")) {
-                    numTel = numTel.replaceFirst("0","+33");
-                }
+            Calendar today = Calendar.getInstance();
+            today.set(Calendar.HOUR, 1);
+            today.set(Calendar.MINUTE, 0);
+            today.add(Calendar.DATE, 1);
+
+            Date dateDebut = today.getTime();
+            today.add(Calendar.DATE, 1);
+            Date dateFin = today.getTime();
+            System.out.println("TWILIO ACCOUNT: " + ACCOUNT_SID);
+            System.out.println("TOKEN: " + AUTH_TOKEN);
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Map<String, String> mapComediensSMS = new HashMap<>();
+            Collection<Comedien> listeComediensNonPrevenus = new ArrayList<>();
+            for (Comedien comedien : merciquimetier.getListeComediensParPeriode(dateDebut, dateFin)) {
+                if (comedien.getNumTel() != null && !comedien.getNumTel().isEmpty()) {
+                    String numTel = comedien.getNumTel();
+                    numTel = numTel.replaceAll("\\s+","");
+                    if (!numTel.startsWith("+33")) {
+                        numTel = numTel.replaceFirst("0","+33");
+                    }
                     Message message = Message.creator(new PhoneNumber(numTel), new PhoneNumber("+17543335908"),
                             this.getSMSBody(comedien, merciquimetier.listeEvenementsParComedienParPeriode(comedien.getId3T(), dateDebut, dateFin), dateDebut)).create();
                     System.out.println("SMS envoyé à " + comedien.getPrenomPersonne() + " " + comedien.getNomPersonne());
                     System.out.println("SID: "+ message.getSid());
                     mapComediensSMS.put(comedien.getPrenomPersonne() + " " + comedien.getNomPersonne(), message.getSid());
-            } else {
-                System.out.println("Le SMS n'a pas pu être envoyé à " + comedien.getPrenomPersonne() + " " + comedien.getNomPersonne());
-                listeComediensNonPrevenus.add(comedien);
+                } else {
+                    System.out.println("Le SMS n'a pas pu être envoyé à " + comedien.getPrenomPersonne() + " " + comedien.getNomPersonne());
+                    listeComediensNonPrevenus.add(comedien);
+                }
             }
+            this.sendAdminNotifMail(mapComediensSMS, listeComediensNonPrevenus);
         }
-        this.sendAdminNotifMail(mapComediensSMS, listeComediensNonPrevenus);
-
+        else {
+            System.out.println("Notification tasks are deactivated");
+        }
     }
 
     private void initTwilioToken() throws IOException {
@@ -93,6 +99,7 @@ public class ReminderTask {
         p.load(reader);
         this.fromEmail = p.getProperty("3T_MAIL");
         this.password = p.getProperty("3T_MAIL_PASSWORD");
+        this.TASK_STATUS = p.getProperty("TASK_STATUS");
     }
 
     private void sendAdminNotifMail(Map<String, String> mapComediensSMS, Collection<Comedien> comediensNonPrevenus) {
@@ -169,41 +176,46 @@ public class ReminderTask {
     }
 
     @Scheduled(cron = "0 0 17 * * SUN")
-    public void sendWeeklyReminder() {
+    public void sendWeeklyReminder() throws IOException {
 
         initMailAccount();
 
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.ssl.trust", "*");
+        if (this.TASK_STATUS.equals("ACTIVATED")) {
 
-        //create Authenticator object to pass in Session.getInstance argument
-        Authenticator auth = new Authenticator() {
-            //override the getPasswordAuthentication method
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(fromEmail, password);
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.ssl.trust", "*");
+
+            //create Authenticator object to pass in Session.getInstance argument
+            Authenticator auth = new Authenticator() {
+                //override the getPasswordAuthentication method
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            };
+            Session session = Session.getInstance(props, auth);
+
+            Calendar today = Calendar.getInstance();
+            today.add(Calendar.DATE, 1);
+
+            Date dateDebut = today.getTime();
+
+            today.add(Calendar.DATE, 6);
+            Date dateFin = today.getTime();
+
+            for (Comedien comedien : merciquimetier.getListeComediensParPeriode(dateDebut, dateFin)) {
+                String toEmail = comedien.getAdresseEmail(); // can be any email id
+                String body = this.getBodyEmail(comedien, merciquimetier.listeEvenementsParComedienParPeriode(comedien.getId3T(), dateDebut, dateFin));
+
+                GestionMailsController.sendEmail(session, toEmail,"RAPPEL : REPRÉSENTATIONS CETTE SEMAINE", body);
             }
-        };
-        Session session = Session.getInstance(props, auth);
-
-        Calendar today = Calendar.getInstance();
-        today.add(Calendar.DATE, 1);
-
-        Date dateDebut = today.getTime();
-
-        today.add(Calendar.DATE, 6);
-        Date dateFin = today.getTime();
-
-        for (Comedien comedien : merciquimetier.getListeComediensParPeriode(dateDebut, dateFin)) {
-            String toEmail = comedien.getAdresseEmail(); // can be any email id
-            String body = this.getBodyEmail(comedien, merciquimetier.listeEvenementsParComedienParPeriode(comedien.getId3T(), dateDebut, dateFin));
-
-            GestionMailsController.sendEmail(session, toEmail,"RAPPEL : REPRÉSENTATIONS CETTE SEMAINE", body);
         }
-
+        else {
+            System.out.println("Notification tasks are deactivated");
+        }
     }
 
     private String getBodyEmail(Comedien com, Collection<Evenement> listeEvenements) {
